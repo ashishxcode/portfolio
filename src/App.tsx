2da187e5adedbd6bb65906b60c51c35b144f9ed6
dipscoder <dipeshjaiswal140@gmail.com>
2022-12-28 17:42:24 +0530

    Fix Router Bug and Add Scrollbar effect

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import Footer from './pages/footer/Footer';
 import FAQ from './pages/faq/FAQ';
 import Features from './pages/feature/Feature';
@@ -6,17 +5,23 @@ import Hero from './pages/hero/Hero';
 import Mentor from './pages/mentors/Mentor';
 import Options from './pages/option/Option';
 import Timeline from './pages/timeline/Timeline';
+import { Routes, Route } from 'react-router-dom';
 
 const App = () => {
 	return (
 		<main>
-			<Hero />
-			{/* <Features /> */}
-			<Timeline />
-			<Options />
-			<Mentor />
-			<FAQ />
-			<Footer />
+			<Routes>
+				<Route path="/" element={<>
+					<Hero />
+					<Features />
+					<Timeline />
+					<Options />
+					<Mentor />
+					<FAQ />
+					<Footer />
+				</>
+				} />
+			</Routes>
 		</main>
 	);
 };
