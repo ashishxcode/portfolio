dec000c69783cae224c74e0f42cc82883b23dacf
cxashish <ashish.culturex@gmail.com>
2022-12-29 09:59:55 +0530

    Adding footer section

--- src/pages/footer/Footer.tsx
+++ src/pages/footer/Footer.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import CreatorXLogo from '../../assets/creatorx.png';
+
 import LinkedinIcon from '../../assets/icons/linkedin.svg';
 import InstagramIcon from '../../assets/icons/instagram.svg';
 import './footer.css';
@@ -8,15 +8,10 @@ const Footer = () => {
 	return (
 		<div id="footer">
 			<div className="container-sm">
-				<div className="footer-header">
-					<h1>unicorn</h1>
-					<h1>X</h1>
-					<img className="logo" alt="" src={CreatorXLogo} />
-				</div>
 				<div className="social-icon-wrapper">
 					<div className="icon-wrapper">
 						<a
-							href="https://linkedin.com/company/culturex-art"
+							href="https://linkedin.com/ashishxcode"
 							target="_blank"
 							rel="noreferrer"
 						>
@@ -24,18 +19,18 @@ const Footer = () => {
 						</a>
 
 						<a
-							href="https://instagram.com/culturex.art"
+							href="https://instagram.com/ashishxcode"
 							target="_blank"
 							rel="noreferrer"
 						>
 							<img src={InstagramIcon} alt="" />
 						</a>
 					</div>
-					<h5 className="username">@CultureX.art</h5>
+					<h5 className="username">@ashishxcode</h5>
 
 					<p>Linkedin | Instagram</p>
 				</div>
-				<h4>Made with ❤️ in Bharat</h4>
+				<h4>EAT | SLEEP | CODE | REPEAT</h4>
 			</div>
 		</div>
 	);
