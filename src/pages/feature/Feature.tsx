f2f4d185a13d20fb2e6c581d7071fc2c5445c0b4
cxashish <ashish.culturex@gmail.com>
2022-12-29 23:41:57 +0530

    style: adding icon for skills

--- src/pages/feature/Feature.tsx
+++ src/pages/feature/Feature.tsx
@@ -1,20 +1,29 @@
 import React from 'react';
 import Marquee from 'react-fast-marquee';
+import CSS from '../../assets/icons/css.png';
+import HTML from '../../assets/icons/html.png';
+import JAVASCRIPT from '../../assets/icons/javascript.png';
+import REACT from '../../assets/icons/react.png';
+import NODE from '../../assets/icons/node.png';
+import TYPESCRIPT from '../../assets/icons/typescript.png';
+import TAILWIND from '../../assets/icons/tailwind.png';
+import BOOTSTRAP from '../../assets/icons/bootstrap.png';
+import MYSQL from '../../assets/icons/mysql.png';
+import POSTGRESQL from '../../assets/icons/postgresql.png';
+
 import './feature.css';
 const Features = () => {
 	const row = [
-		'HTML',
-		'CSS',
-		'JavaScript',
-		'React',
-		'Node',
-		'TypeScript',
-		'Tailwind',
-		'Bootstrap',
-		'Express',
-		'MongoDB',
-		'MYSQL',
-		'POSTGRESQL',
+		<img className="skill-icon" src={HTML} alt="HTML" />,
+		<img className="skill-icon" src={CSS} alt="CSS" />,
+		<img className="skill-icon" src={JAVASCRIPT} alt="JAVASCRIPT" />,
+		<img className="skill-icon" src={REACT} alt="REACT" />,
+		<img className="skill-icon" src={NODE} alt="NODE" />,
+		<img className="skill-icon" src={TYPESCRIPT} alt="TYPESCRIPT" />,
+		<img className="skill-icon" src={TAILWIND} alt="TAILWIND" />,
+		<img className="skill-icon" src={BOOTSTRAP} alt="BOOTSTRAP" />,
+		<img className="skill-icon" src={MYSQL} alt="MYSQL" />,
+		<img className="skill-icon" src={POSTGRESQL} alt="POSTGRESQL" />,
 	];
 	return (
 		<section id="feature">
