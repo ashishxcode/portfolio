274eee3637aa35e06bd38ce7ade0644f211050d1
cxashish <ashish.culturex@gmail.com>
2022-12-29 09:59:39 +0530

    Adding skill section

--- src/pages/feature/Feature.tsx
+++ src/pages/feature/Feature.tsx
@@ -3,32 +3,24 @@ import Marquee from 'react-fast-marquee';
 import './feature.css';
 const Features = () => {
 	const row = [
-		'10-day Bootcamp',
-		'Top-class Mentors',
-		'Guaranteed Growth',
-		'Improved Reach',
-		'Exclusive Creator',
-		'Skill Development',
-		'Creator Centric',
-		'10X Growth',
-		'Community Building',
-		'Networking',
-		'Knowledge Sharing',
-		'Getaway Location',
-		'Interactive Sessions',
-		'Time-bound webinars',
-		'More Opportunities',
-		'Creator’s Retreat',
-		'Collab Openings',
-		'Boosted Development',
-		'Professional Guidance',
-		'Fun Activities',
+		'HTML',
+		'CSS',
+		'JavaScript',
+		'React',
+		'Node',
+		'TypeScript',
+		'Tailwind',
+		'Bootstrap',
+		'Express',
+		'MongoDB',
+		'MYSQL',
+		'POSTGRESQL',
 	];
 	return (
 		<section id="feature">
 			<header className="heading">
-				<span className="text-primary sticker sticker-stroke">Why</span>
-				<h1 className="text-uppercase">unicorn ? </h1>
+				<span className="text-primary sticker sticker-stroke">my</span>
+				<h1 className="text-uppercase">Shields</h1>
 			</header>
 			<div className="feature-wrapper">
 				<Marquee gradientColor={[18, 14, 20]} speed={50}>
@@ -44,18 +36,6 @@ const Features = () => {
 			<div className="feature-wrapper feature-wrapper-wide">
 				<Marquee gradientColor={[18, 14, 20]} speed={50} direction="right">
 					<div className="feature-row row-two">
-						{row.map((item, index) => (
-							<div className="feature-item sticker-stroke" key={index}>
-								{item}
-							</div>
-						))}
-					</div>
-				</Marquee>
-			</div>
-
-			<div className="feature-wrapper">
-				<Marquee gradientColor={[18, 14, 20]} speed={50}>
-					<div className="feature-row row-three">
 						{row.map((item, index) => (
 							<div className="feature-item sticker-stroke" key={index}>
 								{item}
