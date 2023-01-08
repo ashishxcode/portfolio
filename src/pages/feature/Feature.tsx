1849d11a608a55c94d8f8eb245f082a99a521d5e
cxashish <ashish.culturex@gmail.com>
2023-01-08 22:21:43 +0530

    FIX: Repeating icons

--- src/pages/feature/Feature.tsx
+++ src/pages/feature/Feature.tsx
@@ -13,18 +13,22 @@ import POSTGRESQL from '../../assets/icons/postgresql.png';
 
 import './feature.css';
 const Features = () => {
-	const row = [
+	const row1 = [
 		<img className="skill-icon" src={HTML} alt="HTML" />,
 		<img className="skill-icon" src={CSS} alt="CSS" />,
 		<img className="skill-icon" src={JAVASCRIPT} alt="JAVASCRIPT" />,
 		<img className="skill-icon" src={REACT} alt="REACT" />,
 		<img className="skill-icon" src={NODE} alt="NODE" />,
+	];
+
+	const row2 = [
 		<img className="skill-icon" src={TYPESCRIPT} alt="TYPESCRIPT" />,
 		<img className="skill-icon" src={TAILWIND} alt="TAILWIND" />,
 		<img className="skill-icon" src={BOOTSTRAP} alt="BOOTSTRAP" />,
 		<img className="skill-icon" src={MYSQL} alt="MYSQL" />,
 		<img className="skill-icon" src={POSTGRESQL} alt="POSTGRESQL" />,
 	];
+
 	return (
 		<section id="feature">
 			<header className="heading">
@@ -34,7 +38,7 @@ const Features = () => {
 			<div className="feature-wrapper">
 				<Marquee gradientColor={[18, 14, 20]} speed={50}>
 					<div className="feature-row row-one">
-						{row.map((item, index) => (
+						{row1.map((item, index) => (
 							<div className="feature-item sticker-stroke" key={index}>
 								{item}
 							</div>
@@ -45,7 +49,7 @@ const Features = () => {
 			<div className="feature-wrapper feature-wrapper-wide">
 				<Marquee gradientColor={[18, 14, 20]} speed={50} direction="right">
 					<div className="feature-row row-two">
-						{row.map((item, index) => (
+						{row2.map((item, index) => (
 							<div className="feature-item sticker-stroke" key={index}>
 								{item}
 							</div>
